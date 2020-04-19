f41338ea1dae6242119472fadb6d1956e49a4697
WFL <1076281206@qq.com>
2020-04-19 15:02:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiaoyaoyou/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.xiaoyaoyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
